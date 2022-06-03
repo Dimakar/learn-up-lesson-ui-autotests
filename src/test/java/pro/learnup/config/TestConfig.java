30143421c04d4f0b6001ff75569d69d0dea0b4df
dimakar <makarovskii@phystech.edu>
2022-06-03 18:51:15 +0300

    add new config

--- src/test/java/pro/learnup/config/TestConfig.java
+++ src/test/java/pro/learnup/config/TestConfig.java
@@ -3,7 +3,7 @@ package pro.learnup.config;
 import org.aeonbits.owner.Config;
 
 
-@Config.Sources("file:src/test/resources/localconfig.properties")
+@Config.Sources("file:src/test/resources/stageconfig.properties")
 public interface TestConfig extends Config {
     String baseUrl();
     String baseUri();
